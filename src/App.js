8a5570d8b0933a3f3b433f78bcce6653cf051de8
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-01 13:06:15 +0900

    chore: add TaskProvider at root level

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import CompletedTasks from "./components/CompletedTasks";
 import Input from "./components/Input";
 import PendingTasks from "./components/PendingTasks";
+import TasksContextProvider from "./context/TasksContext";
 
 function App() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-bg  text-text">
-      <div className="container w-5/12 h-5/6 p-16 bg-containerBg  rounded-lg shadow-2xl ">
-        <Input />
-        <PendingTasks />
-        <CompletedTasks />
+    <TasksContextProvider>
+      <div className="flex justify-center items-center w-screen h-screen bg-bg  text-text">
+        <div className="container w-5/12 h-5/6 p-16 bg-containerBg  rounded-lg shadow-2xl ">
+          <Input />
+          <PendingTasks />
+          <CompletedTasks />
+        </div>
       </div>
-    </div>
+    </TasksContextProvider>
   );
 }
 
