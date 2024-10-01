533da8af06a40b5c18101cb9a73e03d370447b70
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-01 12:29:02 +0900

    style: finalize PendingTasks Component syles

--- src/components/PendingTasks.jsx
+++ src/components/PendingTasks.jsx
@@ -3,10 +3,12 @@ import Task from "./Task";
 
 export default function PendingTasks() {
   return (
-    <div className="mt-16 ">
-      <h1 className="mb-2">Tasks to do - {"number"}</h1>
-      <Task label="task" />
-      <Task label="task" done={true} />
+    <div className=" mt-16 ">
+      <h1 className="mb-4">Tasks to do - {"number"}</h1>
+      <div className="flex flex-col gap-2">
+        <Task label="task" />
+        <Task label="task" />
+      </div>
     </div>
   );
 }
