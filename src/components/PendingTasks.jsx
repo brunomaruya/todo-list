c58e8d7b88c90ae5e44e65914793a8e8da7cf8b6
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-02 13:41:55 +0900

    feat: filter and display tasks in the PendingTasks component

--- src/components/PendingTasks.jsx
+++ src/components/PendingTasks.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Task from "./Task";
+import { useTasks } from "../context/TasksContext";
 
 export default function PendingTasks() {
+  const { tasks } = useTasks();
   return (
     <div className=" mt-16 ">
       <h1 className="mb-4">Tasks to do - {"number"}</h1>
       <div className="flex flex-col gap-2">
-        <Task label="task" />
-        <Task label="task" />
+        {tasks
+          .filter((task) => !task.done)
+          .map((task, id) => (
+            <Task key={id} label={task.label} />
+          ))}
       </div>
     </div>
   );
